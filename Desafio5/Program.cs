2366db33b4e4623b0e03e950500399e5f263f7b3
JBO21 <jackson.jbo@gmail.com>
2022-01-20 21:18:58 -0300

    Incluindo Desafio5

---
+++ Desafio5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Desafio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contUm = "))";
+            string contDois = "((";
+            string contTres = ")";
+            string contQuatro = "))";
+            string contCinco = "((";
+
+            string[] exer = new string []
+            {
+                contUm,
+                contDois,
+                contTres,
+                contQuatro,
+                contCinco
+            };
+            
+            //string[] lista = new string[]{};
+            
+            
+            for (int i = 0; i < exer.Length; i++)
+            {
+                // string novoitem = exer[i];
+                // List<string> list = new List<string>(exer.ToList());
+                // var check = Array.Exists(list, x=>x==novoitem);
+                // if (check == false)
+                // {
+                //     list.Add(exer[i]);
+                    
+                // }
+
+                
+                
+                
+                // string value = exer[i];
+                // var check = Array.FindIndex(lista, x=>x==value );
+                // if (check == -1)
+                // {
+                //     lista.Append(exer[i]);
+                //     System.Console.WriteLine(lista[i]);
+                // }
+                // else
+                // {
+                // System.Console.WriteLine("Nada adicionado");
+                // } 
+            }
+             
+
+            // foreach (var y in lista)
+            // {
+            //     System.Console.WriteLine(y);
+            // }
+
+           // System.Console.WriteLine(exer[0]);
+
+            // foreach (var j in exer)
+            // {
+            //     System.Console.WriteLine(j);                
+            // }
+            
+
+        }
+    }
+}
